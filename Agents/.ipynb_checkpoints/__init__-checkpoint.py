a3feb459d4f470c4d42da786bfe21d69ae9b256c
AhlamFuad <ahlam@simula.no>
2026-05-06 13:00:28 +0200

    Remove .ipynb_checkpoints from repository

--- Agents/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,6 +0,0 @@
-# Agents/__init__.py
-import logging
-
-# Set up package-wide logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
--- Helpers/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,6 +0,0 @@
-# Helpers/__init__.py
-import logging
-
-# Set up package-wide logging
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
